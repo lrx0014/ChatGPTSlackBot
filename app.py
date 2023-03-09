64c1082ce18232780c83a79a74860dde5f653a54
lrx0014 <lrx0014@hotmail.com>
2023-03-09 15:56:44 +0800

    add dm event handler

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ from revChatGPT.V3 import Chatbot
 from slack_bolt import App
 
 ChatGPTConfig = {
-        "api_key": os.getenv("OPENAI_API_KEY"),
-    }
+    "api_key": os.getenv("OPENAI_API_KEY"),
+}
 if os.getenv("OPENAI_ENGINE"):
     ChatGPTConfig["engine"] = os.getenv("OPENAI_ENGINE")
 
 app = App()
 chatbot = Chatbot(**ChatGPTConfig)
+
 
 # Listen for an event from the Events API
 @app.event("app_mention")
@@ -23,9 +24,9 @@ def event_test(event, say):
         response = chatbot.ask(prompt)
         user = event['user']
         user = f"<@{user}> you asked:"
-        asked = ['>',prompt]
+        asked = ['>', prompt]
         asked = "".join(asked)
-        send = [user,asked,response]
+        send = [user, asked, response]
         send = "\n".join(send)
     except Exception as e:
         print(e)
@@ -37,9 +38,28 @@ def event_test(event, say):
     # Use the `app.event` method to send a reply to the message thread
     say(send, thread_ts=original_message_ts)
 
+
+@app.event("message")
+def event_msg(event, say):
+    prompt = re.sub('(?:\s)<@[^, ]*|(?:^)<@[^, ]*', '', event['text'])
+    try:
+        response = chatbot.ask(prompt)
+        send = response
+    except Exception as e:
+        print(e)
+        send = "We're experiencing exceptionally high demand. Please, try again."
+
+    # Get the `ts` value of the original message
+    original_message_ts = event["ts"]
+
+    # Use the `app.event` method to send a reply to the message thread
+    say(send, thread_ts=original_message_ts)
+
+
 def chatgpt_refresh():
     while True:
         time.sleep(60)
+
 
 if __name__ == "__main__":
     thread = Thread(target=chatgpt_refresh)
